045fae8c84ac41dbe7dc82d21ca0afb8aa96d3b4
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2023-04-25 22:48:52 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "zod.hpp"
+
+using int0to64 = zod<int>::min<0>::max<64>;
+using string20 = zod<std::string>::max<20>;
+using email = zod<std::string>::email;
+
+int main() {
+    int0to64 v = -10;
+    string20 s = "12345678901";
+    email eml = "test@example.co.jp";
+    std::cout << v.value << std::endl;
+
+    std::cout << "Hello, world" << std::endl;
+}
---
+++ zod.hpp
@@ -0,0 +1,190 @@
+#ifndef ZOD_HPP
+#define ZOD_HPP
+
+#include <regex>
+#include <string>
+#include <utility>
+
+template <class T>
+struct empty_annotator {
+    using type = T;
+    const T value;
+
+    template <class... TArgs>
+    empty_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {}
+
+    static void annotate(const T& value) {}
+};
+
+template <class T, int N, class TAnnot=empty_annotator<T>>
+struct max_annotator {
+
+};
+
+template <int N, class TAnnot>
+struct max_annotator<int, N, TAnnot>;
+
+template <int N, class TAnnot>
+struct max_annotator<std::string, N, TAnnot>;
+
+template <class T, int N, class TAnnot=empty_annotator<T>>
+struct min_annotator {
+
+};
+
+template <int N, class TAnnot>
+struct min_annotator<int, N, TAnnot>;
+
+template <int N, class TAnnot>
+struct min_annotator<std::string, N, TAnnot>;
+
+template <int N, class TAnnot>
+struct max_annotator<int, N, TAnnot> {
+    using type = int;
+    const int value;
+    
+    template <int M>
+    using min = min_annotator<int, M, TAnnot>;
+
+    template <class... TArgs>
+    max_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_annotator<int, N, TAnnot>::annotate(value);
+    }
+
+    static void annotate(const int& value) {
+        if(value > N) {
+            throw 0;
+        }
+        TAnnot::annotate(value);
+    }
+};
+
+template <int N, class TAnnot>
+struct min_annotator<int, N, TAnnot> {
+    using type = int;
+    const int value;
+
+    template <int M>
+    using max = max_annotator<int, M, TAnnot>;
+
+    template <class... TArgs>
+    min_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_annotator<int, N, TAnnot>::annotate(value);
+    }
+
+    static void annotate(const std::string& value) {
+        if(value.length() < N) {
+            throw 0;
+        }
+        TAnnot::annotate(value);
+    }
+};
+
+template <class TAnnot>
+struct email_annotator;
+
+template <int N, class TAnnot>
+struct max_annotator<std::string, N, TAnnot> {
+    using type = std::string;
+    const std::string value;
+    
+    template <int M>
+    using min = min_annotator<std::string, M, TAnnot>;
+    using email = email_annotator<TAnnot>;
+
+    template <class... TArgs>
+    max_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_annotator<std::string, N, TAnnot>::annotate(value);
+    }
+
+    static void annotate(const std::string& value) {
+        if(value.length() > N) {
+            throw 0;
+        }
+        TAnnot::annotate(value);
+    }
+};
+
+template <int N, class TAnnot>
+struct min_annotator<std::string, N, TAnnot> {
+    using type = std::string;
+    const std::string value;
+
+    template <int M>
+    using max = max_annotator<std::string, M, TAnnot>;
+    using email = email_annotator<TAnnot>;
+
+    template <class... TArgs>
+    min_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_annotator<std::string, N, TAnnot>::annotate(value);
+    }
+
+    static void annotate(const std::string& value) {
+        if(value.length() < N) {
+            throw 0;
+        }
+        TAnnot::annotate(value);
+    }
+};
+
+template <class TAnnot=empty_annotator<std::string>>
+struct email_annotator {
+    using type = std::string;
+    const std::string value;
+
+    template <int M>
+    using max = max_annotator<std::string, M, TAnnot>;
+    template <int N>
+    using min = min_annotator<std::string, N>;
+
+    template <class... TArgs>
+    email_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        email_annotator<>::annotate(value);
+    }
+
+    static void annotate(const std::string& value) {
+        static std::regex re(R"*(^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$)*");
+        std::smatch m;
+        if(!std::regex_match(value, m, re)) {
+            throw 0;
+        }
+        TAnnot::annotate(value);
+    }
+
+};
+
+template <class T> 
+struct zod {
+
+};
+
+template <> 
+struct zod<int> {
+    using type = int;
+    const int value;
+
+    template <int N>
+    using max = max_annotator<int, N>;
+    template <int N>
+    using min = min_annotator<int, N>;
+
+    template <class... TArgs>
+    zod(TArgs... args) : value(std::forward<TArgs>(args)...) {}
+};
+
+template <> 
+struct zod<std::string> {
+    using type = std::string;
+    const std::string value;
+
+    template <int N>
+    using max = max_annotator<std::string, N>;
+    template <int N>
+    using min = min_annotator<std::string, N>;
+    using email = email_annotator<>;
+
+    template <class... TArgs>
+    zod(TArgs... args) : value(std::forward<TArgs>(args)...) {}
+};
+
+#endif
