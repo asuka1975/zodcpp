d1264148f10561865f0160215c100ccbfe2875f9
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2023-04-25 23:18:47 +0900

    annotation関数が再帰的に呼び出されないバグを修正

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 using int0to64 = zod<int>::min<0>::max<64>;
 using string20 = zod<std::string>::max<20>;
 using email = zod<std::string>::email;
+using email10 = zod<std::string>::email::max<10>;
 
 int main() {
-    int0to64 v = -70;
+    int0to64 v = 60;
     string20 s = "12345678901";
     email eml = "test@example.co.jp";
+    email10 e2 = "test@co.jp";
     std::cout << v.value << std::endl;
 
     std::cout << "Hello, world" << std::endl;
