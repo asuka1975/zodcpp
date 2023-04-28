b66831cfce5e5ebef5795063df6107f3c5d14a76
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2023-04-28 16:16:00 +0900

    wip

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "zod.hpp"
 
 using int0to64 = zod<int>::min<0>::max<64>;
-using string20 = zod<std::string>::max<20>;
+using string10 = zod<std::string>::max<10>;
 using email = zod<std::string>::email;
 using email10 = zod<std::string>::email::max<10>;
 
+
 int main() {
-    int0to64 v = 60;
-    string20 s = "12345678901";
+    constexpr int0to64 v = 50;
+    constexpr std::size_t n = string10("10").value.size();
     email eml = "test@example.co.jp";
     email10 e2 = "test@co.jp";
     std::cout << v.value << std::endl;
-
-    std::cout << "Hello, world" << std::endl;
 }
