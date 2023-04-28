a95dc1e76c143a33c38899a90c582d3e1739799c
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2023-04-28 16:33:24 +0900

    アノテーションをバリデーションに変更

--- zod.hpp
+++ zod.hpp
@@ -6,185 +6,185 @@
 #include <utility>
 
 template <class T>
-struct empty_annotator {
+struct empty_validator {
     using type = T;
     const T value;
-    using this_t = empty_annotator<T>;
+    using this_t = empty_validator<T>;
 
     template <class... TArgs>
-    constexpr empty_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {}
+    constexpr empty_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {}
 
-    constexpr static void annotate(const T& value) {}
+    constexpr static void validate(const T& value) {}
 };
 
-template <class T, int N, class TAnnot=empty_annotator<T>>
-struct max_annotator {
-
-};
-
-template <int N, class TAnnot>
-struct max_annotator<int, N, TAnnot>;
-
-template <int N, class TAnnot>
-struct max_annotator<std::string, N, TAnnot>;
-
-template <class T, int N, class TAnnot=empty_annotator<T>>
-struct min_annotator {
+template <class T, int N, class TValid=empty_validator<T>>
+struct max_validator {
 
 };
 
-template <int N, class TAnnot>
-struct min_annotator<int, N, TAnnot>;
+template <int N, class TValid>
+struct max_validator<int, N, TValid>;
 
-template <int N, class TAnnot>
-struct min_annotator<std::string, N, TAnnot>;
+template <int N, class TValid>
+struct max_validator<std::string, N, TValid>;
 
-template <int N, class TAnnot>
-struct max_annotator<int, N, TAnnot> {
+template <class T, int N, class TValid=empty_validator<T>>
+struct min_validator {
+
+};
+
+template <int N, class TValid>
+struct min_validator<int, N, TValid>;
+
+template <int N, class TValid>
+struct min_validator<std::string, N, TValid>;
+
+template <int N, class TValid>
+struct max_validator<int, N, TValid> {
     using type = int;
-    using this_t = max_annotator<int, N, TAnnot>;
+    using this_t = max_validator<int, N, TValid>;
     const int value;
     
     template <int M>
-    using min = min_annotator<int, M, this_t>;
+    using min = min_validator<int, M, this_t>;
 
     template <class... TArgs>
-    constexpr max_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
-        max_annotator<int, N, TAnnot>::annotate(value);
+    constexpr max_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_validator<int, N, TValid>::validate(value);
     }
 
-    constexpr static void annotate(const int& value) {
+    constexpr static void validate(const int& value) {
         if(value > N) {
             throw 0;
         }
-        TAnnot::annotate(value);
+        TValid::validate(value);
     }
 };
 
-template <int N, class TAnnot>
-struct min_annotator<int, N, TAnnot> {
+template <int N, class TValid>
+struct min_validator<int, N, TValid> {
     using type = int;
-    using this_t = min_annotator<int, N, TAnnot>;
+    using this_t = min_validator<int, N, TValid>;
     const int value;
 
     template <int M>
-    using max = max_annotator<int, M, this_t>;
+    using max = max_validator<int, M, this_t>;
 
     template <class... TArgs>
-    constexpr min_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
-        max_annotator<int, N, TAnnot>::annotate(value);
+    constexpr min_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_validator<int, N, TValid>::validate(value);
     }
 
-    constexpr static void annotate(const int& value) {
+    constexpr static void validate(const int& value) {
         if(value < N) {
             throw 0;
         }
-        TAnnot::annotate(value);
+        TValid::validate(value);
     }
 };
 
-template <class TAnnot>
-struct email_annotator;
+template <class TValid>
+struct email_validator;
 
-template <class TAnnot>
-struct url_annotator;
+template <class TValid>
+struct url_validator;
 
-template <int N, class TAnnot>
-struct max_annotator<std::string, N, TAnnot> {
+template <int N, class TValid>
+struct max_validator<std::string, N, TValid> {
     using type = std::string;
-    using this_t = max_annotator<std::string, N, TAnnot>;
+    using this_t = max_validator<std::string, N, TValid>;
     const std::string value;
     
     template <int M>
-    using min = min_annotator<std::string, M, this_t>;
-    using email = email_annotator<this_t>;
+    using min = min_validator<std::string, M, this_t>;
+    using email = email_validator<this_t>;
 
     template <class... TArgs>
-    constexpr max_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
-        max_annotator<std::string, N, TAnnot>::annotate(value);
+    constexpr max_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_validator<std::string, N, TValid>::validate(value);
     }
 
-    constexpr static void annotate(const std::string& value) {
+    constexpr static void validate(const std::string& value) {
         if(value.length() > N) {
             throw 0;
         }
-        TAnnot::annotate(value);
+        TValid::validate(value);
     }
 };
 
-template <int N, class TAnnot>
-struct min_annotator<std::string, N, TAnnot> {
+template <int N, class TValid>
+struct min_validator<std::string, N, TValid> {
     using type = std::string;
-    using this_t = min_annotator<std::string, N, TAnnot>;
+    using this_t = min_validator<std::string, N, TValid>;
     const std::string value;
 
     template <int M>
-    using max = max_annotator<std::string, M, this_t>;
-    using email = email_annotator<this_t>;
+    using max = max_validator<std::string, M, this_t>;
+    using email = email_validator<this_t>;
 
     template <class... TArgs>
-    constexpr min_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
-        max_annotator<std::string, N, TAnnot>::annotate(value);
+    constexpr min_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        max_validator<std::string, N, TValid>::validate(value);
     }
 
-    constexpr static void annotate(const std::string& value) {
+    constexpr static void validate(const std::string& value) {
         if(value.length() < N) {
             throw 0;
         }
-        TAnnot::annotate(value);
+        TValid::validate(value);
     }
 };
 
-template <class TAnnot=empty_annotator<std::string>>
-struct email_annotator {
+template <class TValid=empty_validator<std::string>>
+struct email_validator {
     using type = std::string;
-    using this_t = email_annotator<TAnnot>;
+    using this_t = email_validator<TValid>;
     const std::string value;
 
     template <int M>
-    using max = max_annotator<std::string, M, this_t>;
+    using max = max_validator<std::string, M, this_t>;
     template <int M>
-    using min = min_annotator<std::string, M, this_t>;
+    using min = min_validator<std::string, M, this_t>;
 
     template <class... TArgs>
-    constexpr email_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
-        email_annotator<>::annotate(value);
+    constexpr email_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        email_validator<>::validate(value);
     }
 
-    static void annotate(const std::string& value) {
+    static void validate(const std::string& value) {
         static std::regex re(R"*(^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$)*");
         std::smatch m;
         if(!std::regex_match(value, m, re)) {
             throw 0;
         }
-        TAnnot::annotate(value);
+        TValid::validate(value);
     }
 
 };
 
-template <class TAnnot=empty_annotator<std::string>>
-struct url_annotator {
+template <class TValid=empty_validator<std::string>>
+struct url_validator {
     using type = std::string;
-    using this_t = email_annotator<TAnnot>;
+    using this_t = email_validator<TValid>;
     const std::string value;
 
     template <int M>
-    using max = max_annotator<std::string, M, this_t>;
+    using max = max_validator<std::string, M, this_t>;
     template <int M>
-    using min = min_annotator<std::string, M, this_t>;
+    using min = min_validator<std::string, M, this_t>;
 
     template <class... TArgs>
-    constexpr url_annotator(TArgs... args) : value(std::forward<TArgs>(args)...) {
-        url_annotator<>::annotate(value);
+    constexpr url_validator(TArgs... args) : value(std::forward<TArgs>(args)...) {
+        url_validator<>::validate(value);
     }
 
-    static void annotate(const std::string& value) {
+    static void validate(const std::string& value) {
         static std::regex re(R"*(\w+://[\w!?/+\-_~;.,*&@#$%()'[\]]+)*");
         std::smatch m;
         if(!std::regex_match(value, m, re)) {
             throw 0;
         }
-        TAnnot::annotate(value);
+        TValid::validate(value);
     }
 };
 
@@ -199,9 +199,9 @@ struct zod<int> {
     const int value;
 
     template <int N>
-    using max = max_annotator<int, N>;
+    using max = max_validator<int, N>;
     template <int N>
-    using min = min_annotator<int, N>;
+    using min = min_validator<int, N>;
 
     template <class... TArgs>
     constexpr zod(TArgs... args) : value(std::forward<TArgs>(args)...) {}
@@ -213,11 +213,11 @@ struct zod<std::string> {
     const std::string value;
 
     template <int N>
-    using max = max_annotator<std::string, N>;
+    using max = max_validator<std::string, N>;
     template <int N>
-    using min = min_annotator<std::string, N>;
-    using email = email_annotator<>;
-    using url = url_annotator<>;
+    using min = min_validator<std::string, N>;
+    using email = email_validator<>;
+    using url = url_validator<>;
 
     template <class... TArgs>
     constexpr zod(TArgs... args) : value(std::forward<TArgs>(args)...) {}
